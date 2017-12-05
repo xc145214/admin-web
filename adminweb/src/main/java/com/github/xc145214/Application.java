932246650b2f82701287a2f3c87811af3cff21c6
夏川 <xiachuan@my089.net>
2017-12-05 11:24:48 +0800

    init base java files

---
+++ adminweb/src/main/java/com/github/xc145214/Application.java
@@ -0,0 +1,13 @@
+package com.github.xc145214;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+    public static void main(String[] args) {
+
+        SpringApplication.run(Application.class, args);
+    }
+}
---
+++ adminweb/src/main/java/com/github/xc145214/web/IndexController.java
@@ -0,0 +1,42 @@
+/*************************************************************************
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *
+ *                COPYRIGHT (C) HONGLING CAPITAL CORPORATION 2012
+ *    ALL RIGHTS RESERVED BY HONGLING CAPITAL CORPORATION. THIS PROGRAM
+ * MUST BE USED  SOLELY FOR THE PURPOSE FOR WHICH IT WAS FURNISHED BY
+ * HONGLING CAPITAL CORPORATION. NO PART OF THIS PROGRAM MAY BE REPRODUCED
+ * OR DISCLOSED TO OTHERS,IN ANY FORM, WITHOUT THE PRIOR WRITTEN
+ * PERMISSION OF HONGLING CAPITAL CORPORATION. USE OF COPYRIGHT NOTICE
+ * DOES NOT EVIDENCE PUBLICATION OF THE PROGRAM.
+ *                  HONGLING CAPITAL CONFIDENTIAL AND PROPRIETARY
+ *************************************************************************/
+package com.github.xc145214.web;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ *
+ *
+ * @author xiachuan at 2017/12/5 11:19。
+ */
+@Controller
+public class IndexController {
+
+
+    @RequestMapping(
+            value = {"/index", "/"},
+            method = RequestMethod.GET)
+    public ModelAndView index(HttpServletRequest request, HttpServletResponse response){
+        String viewName = "index";
+        ModelAndView view = new ModelAndView(viewName);
+        return view;
+    }
+
+}
+
